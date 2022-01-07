4995418c02096b85722ad8c5eef858799ddf4229
Ole Vølund Skov Mortensen <olevsmortensen@dbc5radio.dk>
2022-01-07 21:55:49 +0100

    adjusted to targetthe right numbers

--- index.py
+++ index.py
@@ -3,13 +3,12 @@ import pandas as pd
 import re
 def Danske_Nummer(falsk_streng):
     return int(falsk_streng.replace(".","").replace(",","."))
-
 file = input("Angiv Fil:")
 df = tb.read_pdf(file, pages='4', stream=True)
 ncolumns = df[0].iloc[1,[0,1,2,4,5,6]]
 df1 = df[0].iloc[3:,:6]
 df1.columns = ncolumns
-netto_renteIntægter = df1.iloc[0,1]
+netto_renteIntægter = df1.iloc[5,1]
 resultat_før_skat = df1.iloc[11,1]
 resultat_før_skat = Danske_Nummer(resultat_før_skat)
 netto_renteIntægter = Danske_Nummer(netto_renteIntægter)
